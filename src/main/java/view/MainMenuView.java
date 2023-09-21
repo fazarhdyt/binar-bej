d1274f916be7bb0be93227876a0701f1f0cf515d
Muhammad Fazar Hidayat <m.fajar184@gmail.com>
2023-09-21 19:35:07 +0700

    refactor MainMenuView

--- src/main/java/view/MainMenuView.java
+++ src/main/java/view/MainMenuView.java
@@ -13,25 +13,27 @@ import java.util.Optional;
 
 public class MainMenuView {
 
-    IMenuService menuService;
-    IShopService shopService;
-    IValidationService validationService;
+    private final IMenuService menuService;
+    private final IShopService shopService;
+    private final IValidationService validationService;
+
+    private final String regexNumeric = "[0-9]+";
+    private final String regexMenuConfirmAndPayment = "^[0-2]$";
+    private final List<Product> listProducts;
+    private final List<CartItem> listCartItems;
 
     public MainMenuView(IMenuService menuService, IShopService shopService, IValidationService validationService) {
         this.menuService = menuService;
         this.shopService = shopService;
         this.validationService = validationService;
+        this.listProducts = ProductRepository.getProducts();
+        this.listCartItems = new ArrayList<>();
     }
 
-    int choose;
-    int qty;
-    boolean isLooping = true;
-    String regexNumeric = "[0-9]+";
-    String regexMenuConfirmAndPayment = "^[0-2]$";
-    List<Product> listProducts = ProductRepository.getProducts();
-    List<CartItem> listCartItems = new ArrayList<>();
-
     public void runApp() {
+        int choose;
+        boolean isLooping = true;
+
         do {
             try {
                 menuService.printMenu(listProducts, "Selamat datang di Warkop Top Global");
@@ -43,43 +45,57 @@ public class MainMenuView {
             }
             switch (choose) {
                 case 0:
-                    System.exit(0);
+                    isLooping = false;
                     break;
                 case 99:
-                    menuService.printConfirmAndPayment(listCartItems);
-                    choose = Integer.parseInt(validationService.inputUser("=> ", "masukkan hanya dengan angka 0-2", regexMenuConfirmAndPayment));
-                    if (choose == 1) {
-                        if (listCartItems.isEmpty()) {
-                            menuService.failGenerateReceipt();
-                            validationService.inputUser("tekan enter untuk kembali ke menu utama", null, "^$|^.+$");
-                        } else {
-                            menuService.generateReceipt(listCartItems, "Warkop Top Global");
-                            isLooping = false;
-                        }
-                    } else if (choose == 2) {
-                        break;
-                    } else if (choose == 0) {
-                        System.exit(0);
-                    }
+                    handleConfirmAndPayment();
                     break;
                 default:
-                    try {
-                        Optional<Product> product = shopService.getProductById(listProducts, choose);
-                        if (product.isPresent()) {
-                            Product selectedProduct = product.get();
-                            menuService.printConfirmOrder(selectedProduct);
-                            qty = Integer.parseInt(validationService.inputUser("qty => ", "masukkan hanya dengan angka positif", regexNumeric));
-                            if (qty != 0) {
-                                shopService.addToCartShop(listCartItems, selectedProduct, qty);
-                            }
-                        }
-                    } catch (NumberFormatException nfe) {
-                        System.err.println("Error: " + nfe.getMessage());
-                        System.err.println("Masukkan dengan angka tidak melebihi batas integer!");
-                    }
-
+                    handleProductSelection(choose);
             }
         } while (isLooping);
+    }
+
+    public void handleConfirmAndPayment() {
+        menuService.printConfirmAndPayment(listCartItems);
+        int choose = Integer.parseInt(validationService.inputUser("=> ", "masukkan hanya dengan angka 0-2", regexMenuConfirmAndPayment));
+
+        switch (choose) {
+            case 1:
+                if (listCartItems.isEmpty()) {
+                    menuService.failGenerateReceipt();
+                    validationService.inputUser("tekan enter untuk kembali ke menu utama", null, "^$|^.+$");
+                } else {
+                    menuService.generateReceipt(listCartItems, "Warkop Top Global");
+                    System.exit(0);
+                }
+                break;
+            case 2:
+                break;
+            case 0:
+                System.exit(0);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void handleProductSelection(int productId) {
+        try {
+            Optional<Product> product = shopService.getProductById(listProducts, productId);
+            if (product.isPresent()) {
+                Product selectedProduct = product.get();
+                menuService.printConfirmOrder(selectedProduct);
+                int qty = Integer.parseInt(validationService.inputUser("qty => ", "masukkan hanya dengan angka positif", regexNumeric));
+
+                if (qty > 0) {
+                    shopService.addToCartShop(listCartItems, selectedProduct, qty);
+                }
+            }
+        } catch (NumberFormatException nfe) {
+            System.err.println("Error: " + nfe.getMessage());
+            System.err.println("Masukkan dengan angka tidak melebihi batas integer!");
+        }
     }
 
 }
