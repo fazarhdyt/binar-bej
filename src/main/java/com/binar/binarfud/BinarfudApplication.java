0e9aaeffa4c25690f3a38e2def236fda0a6a20b5
Muhammad Fazar Hidayat <m.fajar184@gmail.com>
2023-10-20 23:37:16 +0700

    add jpa auditing

--- src/main/java/com/binar/binarfud/BinarfudApplication.java
+++ src/main/java/com/binar/binarfud/BinarfudApplication.java
@@ -2,8 +2,10 @@ package com.binar.binarfud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BinarfudApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/binar/binarfud/model/Order.java
+++ src/main/java/com/binar/binarfud/model/Order.java
@@ -1,10 +1,14 @@
 package com.binar.binarfud.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -17,6 +21,8 @@ import java.util.List;
 @Builder
 @Entity
 @Table(name = "orders")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"orderTime", "completionTime"}, allowGetters = true)
 public class Order implements Serializable {
 
     @Id
@@ -27,7 +33,16 @@ public class Order implements Serializable {
     )
     private String id;
 
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
     private Date orderTime;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date completionTime;
+
     private String destinationAddress;
     private boolean completed;
 
@@ -35,6 +50,6 @@ public class Order implements Serializable {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "order", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE)
     private List<OrderDetail> orderDetails;
 }
